1b411e393a8d22b0660e62a77dd2c8f67ac963fc
shariqshamsi <shariqtanzeem@hotmail.com>
2024-03-22 11:55:00 +0500

    Django Image Uploader is a basic website built using Django that allows users to upload and view images. The website includes functionality for uploading images through the admin panel or directly from the website. Superuser access is provided for administrative tasks such as managing uploaded images.

---
+++ django_image_uploader/image_uploader/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .forms import ImageForm
+from .models import Image
+
+def home(request):
+    if request.method == "POST":
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    form = ImageForm()
+    img = Image.objects.all()
+    context = {'form':form, 'img':img}
+    return render(request, 'myapp/home.html', context)
